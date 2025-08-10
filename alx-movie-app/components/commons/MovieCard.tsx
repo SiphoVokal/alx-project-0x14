d31adb1648e7fae2997fff38b2613ddd1671aaf0
SiphoVokal <163132466+SiphoVokal@users.noreply.github.com>
2025-08-10 20:14:01 +0200

    setup

---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MovieCard: React.FC = () => {
+  return (
+    <></>
+  );
+};
+
+export default MovieCard;
---
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Layout: React.FC = () => {
+  return (
+    <></>
+  );
+};
+
+export default Layout;
